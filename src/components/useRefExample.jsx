9593193f86c7cd3b151b6a59ffc3590452dd0980
srinu1544 <srinu1544@gmail.com>
2022-11-30 15:37:22 +0530

    useRefExampleAdded

---
+++ src/components/useRefExample.jsx
@@ -0,0 +1,28 @@
+import React,{useRef,useEffect} from 'react'
+
+function UseRefExample() {
+
+  const data = useRef(null);
+
+const submitHandler = e => {
+  e.preventDefault();
+  console.log(data.current.value);
+}
+
+useEffect(()=>{
+  data.current.focus()
+},[])
+
+  return (
+    <div >
+    <center>
+      <form onSubmit={submitHandler}>
+        <input ref={data} type='text' placeholder='enter'></input> <br></br>
+        <input type='submit'></input>
+      </form>
+    </center>
+    </div>
+  );
+}
+
+export default UseRefExample;
